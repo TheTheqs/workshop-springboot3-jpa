6476fb08a35877c7c3744dd0659f34ef778f3b09
Matheqs <matheqs@outlook.com>
2024-04-02 04:53:38 -0300

    Payment addition

--- src/main/java/com/husnhus/test/config/TestConfig.java
+++ src/main/java/com/husnhus/test/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.husnhus.test.entities.Category;
 import com.husnhus.test.entities.Order;
 import com.husnhus.test.entities.OrderItem;
+import com.husnhus.test.entities.Payment;
 import com.husnhus.test.entities.Product;
 import com.husnhus.test.entities.User;
 import com.husnhus.test.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment (null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 
